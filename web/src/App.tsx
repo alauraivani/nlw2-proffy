187694c805fe5236ae2b60e3a8a6b5d92ea688ce
Anna Laura <annalaura@pop-os.localdomain>
2020-10-09 19:16:41 -0300

    feat: add Landing page component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './assets/styles/global.css';
+import Landing from './pages/Landing/index';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Again!</h1>
+      <Landing/>
     </div>
   );
 }
