69bf0e02f6c24f1f2fb90d07b4e376d8508935b9
Anna Laura <annalaura@pop-os.localdomain>
2020-10-10 10:39:45 -0300

    feat: add TeacherList page component

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function TeacherList() {
+  return (
+    <div id='page-teacher-list' className='container'>
+     
+    </div>
+  );
+}
+
+export default TeacherList;
