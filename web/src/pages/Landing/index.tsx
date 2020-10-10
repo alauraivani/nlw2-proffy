70a5ef7fedf07ca0f2020ac272e587071d00efd7
Anna Laura <annalaura@pop-os.localdomain>
2020-10-10 10:50:58 -0300

    feat: add and config routes.tsx

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -22,13 +23,14 @@ function Landing() {
           className='hero-image'
         />
         <div className='buttons-container'>
- 
+        <Link to='/study' className='study'>
             <img src={studyIcon} alt='Estudar' />
             Estudar
-      
-
+          </Link>
+        <Link to='/give-classes' className='give-classes'>
             <img src={giveClassesIcon} alt='Dar aulas' />
             Dar aulas
+        </Link>
 
         </div>
         <span className='total-connections'>
